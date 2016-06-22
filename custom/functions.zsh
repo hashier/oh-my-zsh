9b93b0b00b5f926cdd66450f5502acc2d7c2808f
Christopher Loessl <cloessl+github@gmail.com>
2016-06-22 11:47:14 +0200

    Add: transfer.sh

--- custom/functions.zsh
+++ custom/functions.zsh
@@ -15,3 +15,21 @@ function gitgrepi () { git grep -i $1 $(git rev-list --all) }
 
 # http://stackoverflow.com/questions/2363197/can-i-get-a-list-of-files-marked-assume-unchanged
 function gitassumeunchanged () { git ls-files -v `git rev-parse --show-toplevel` | grep "^[a-z]" }
+
+function transfer() {
+if [ $# -eq 0 ]
+then
+    echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+    return 1
+fi
+tmpfile=$( mktemp -t transferXXX )
+if tty -s
+then
+    basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+    curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile
+else
+    curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile
+fi
+cat $tmpfile
+rm -f $tmpfile
+}
